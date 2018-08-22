177d4d7b959c077d02560c294784658cd78efba6
Rain_ye3939 <305171397@qq.com>
2018-08-22 21:52:51 +0800

    主程序建立

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// #include <pthread.h>
+#include <winsock2.h>
+#include <stdlib.h>
+#include <unistd.h>	
+// #include <sched.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include "cJSON.c"
+
+main(int argc, char const *argv[])
+{
+    puts("MA LE GE B!");
+    getchar();
+    return 0;
+}
